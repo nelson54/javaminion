879d39dddb6c3252b1d17507ae71d127518f2511
Derek Nelson <dereknelson01@gmail.com>
2015-03-14 16:42:55 -0400

    Add market test

--- src/test/groovy/com/github/nelson54/dominion/cards/MarketTest.groovy
+++ src/test/groovy/com/github/nelson54/dominion/cards/MarketTest.groovy
@@ -17,6 +17,15 @@ class MarketTest extends GameTest {
 
     @Test
     void testApply() {
+        ActionCard card = (ActionCard)game.giveCardToPlayer("Market", player)
 
+        int startingHandSize = player.hand.size()
+        int startingMoney = player.currentTurn.getMoneyPool()
+        card.apply(player, game)
+
+        assert player.hand.size() == startingHandSize+1, "Playing should draw a card"
+        assert player.currentTurn.getMoneyPool() >= startingMoney+1, "Should have an extra money"
+        assert player.currentTurn.getBuyPool() == 2, "Should have an extra buy"
+        assert player.currentTurn.getActionPool() == 2, "Should have an extra action"
     }
 }
