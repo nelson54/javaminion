79dc5bf271004111b068545235b96d99762e7459
Derek <dereknelson01@gmail.com>
2015-03-19 10:55:10 -0400

    Needed to update dist

--- src/main/js/app/scripts/app.js
+++ src/main/js/app/scripts/app.js
@@ -9,8 +9,8 @@
  * Main module of the application.
  */
 
-var baseUrl = "http://localhost:9001";
-//var baseUrl = "";
+//var baseUrl = "http://localhost:9001";
+var baseUrl = "";
 
 angular
   .module('dominionFrontendApp', [
