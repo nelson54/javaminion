88c799a1092aa0f0c4f47761e15505bac57258d8
Derek <dereknelson01@gmail.com>
2015-04-17 21:55:11 -0400

    fix to make remodel work with new choice setup

--- src/main/java/com/github/nelson54/dominion/web/WebMvcConfig.java
+++ src/main/java/com/github/nelson54/dominion/web/WebMvcConfig.java
@@ -1,63 +1,18 @@
 package com.github.nelson54.dominion.web;
 
-import com.github.nelson54.dominion.web.users.UserInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.social.connect.UsersConnectionRepository;
-import org.springframework.social.connect.web.thymeleaf.SpringSocialDialect;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
-
-import javax.inject.Inject;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserInterceptor(usersConnectionRepository));
-    }
-
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/signin");
-        registry.addViewController("/signout");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
     }
-
-/*    @Bean
-    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine);
-        return viewResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine(TemplateResolver templateResolver) {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver);
-        templateEngine.addDialect(new SpringSocialDialect());
-        return templateEngine;
-    }
-
-    @Bean
-    public TemplateResolver templateResolver() {
-        TemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("/views/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML5");
-        templateResolver.setCacheable(false);
-
-        return templateResolver;
-    }*/
-
-    private @Inject UsersConnectionRepository usersConnectionRepository;
 
 }
