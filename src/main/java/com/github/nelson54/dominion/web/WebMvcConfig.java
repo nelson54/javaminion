29e91388226c959eaf36a032d02d1894084e281a
Derek Nelson <dcnelson@zixcorp.com>
2018-12-28 13:43:50 -0500

    activation

--- src/main/java/com/github/nelson54/dominion/web/WebMvcConfig.java
+++ src/main/java/com/github/nelson54/dominion/web/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.github.nelson54.dominion.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebMvc
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
@@ -18,9 +20,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/**/*")
-                .addResourceLocations("/resources/public/");
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 
 }
