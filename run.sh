6d6ded7f0412f0c9ae0f1783312fecddc70298f0
Derek Nelson <dcnelson@zixcorp.com>
2019-02-06 14:57:44 -0500

    update environment bootstrap

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 cd /build/
-tar -xvzf /build/front-end.tar.gz
+tar -xvzf ./front-end.tar.gz
 
 cd /build/dist
 cp -R * /app/src/main/resources/public
 
+cd /app
 bash ./gradlew -Dskip.tests bootRun
