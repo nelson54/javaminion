5f3526243f025760f1d798a7fdd21be1f5a173c6
Derek Nelson <dcnelson@zixcorp.com>
2019-03-01 12:45:51 -0500

    copy test results and test-reports after build

--- scripts/copy_test_log_from_docker.sh
+++ scripts/copy_test_log_from_docker.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-docker exec $(docker ps -a -q -f "name=javaminion_app" | head -n 1) bash /app/scripts/create-test-archives.sh
-
-docker cp $(docker ps -a -q -f "name=javaminion_app" | head -n 1):/app/build/reports/tests/test/index.html /root/
-
-docker cp $(docker ps -a -q -f "name=javaminion_app" | head -n 1):/app/build/test-results/test-results.tar.gz /root/
-
-docker cp $(docker ps -a -q -f "name=javaminion_app" | head -n 1):/app/build/reports/test-reports.tar.gz /root/
+docker-compose exec app bash /app/scripts/create-test-archives.sh
--- scripts/create-test-archives.sh
+++ scripts/create-test-archives.sh
@@ -5,3 +5,6 @@ tar -xvzf ./test-results.tar.gz
 
 cd /app/build/reports
 tar -xvzf ./test-reports.tar.gz
+
+mv ./test-results.tar.gz /archives/
+mv ./test-reports.tar.gz /archives/
