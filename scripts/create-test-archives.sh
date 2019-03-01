5c4744d8fa24fac9a2c2f10dc9907afa5f0284c7
Derek Nelson <dcnelson@zixcorp.com>
2019-03-01 13:27:53 -0500

    copy test results and test-reports after build

--- scripts/create-test-archives.sh
+++ scripts/create-test-archives.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 cd /app/build/test-results
-tar -xvzf ./test-results.tar.gz
+tar -cvzf ./test-results.tar.gz .
+mv ./test-results.tar.gz /archives/
 
 cd /app/build/reports
-tar -xvzf ./test-reports.tar.gz
-
-mv ./test-results.tar.gz /archives/
+tar -cvzf ./test-reports.tar.gz .
 mv ./test-reports.tar.gz /archives/
