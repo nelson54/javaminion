c7d88db19e737048c3bc4358244079f70fd59174
Derek Nelson <dcnelson@zixcorp.com>
2019-03-01 16:19:30 -0500

    do archive and use publish html

--- scripts/create-test-archives.sh
+++ scripts/create-test-archives.sh
@@ -2,8 +2,10 @@
 
 cd /app/build/test-results
 tar -cvzf ./test-results.tar.gz .
+chmod 777 ./test-results.tar.gz
 mv ./test-results.tar.gz /archives/
 
 cd /app/build/reports
 tar -cvzf ./test-reports.tar.gz .
+chmod 777 ./test-reports.tar.gz
 mv ./test-reports.tar.gz /archives/
