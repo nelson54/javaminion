855f9e5fa66eedcdeddc561f17526fe52a251566
Derek Nelson <dcnelson@zixcorp.com>
2019-04-01 11:39:17 -0400

    Upgrading to angular 1.5 - adding new .

--- src/main/js/app/components/login/login-component.js
+++ src/main/js/app/components/login/login-component.js
@@ -4,15 +4,9 @@ let dominionFrontendApp = angular.module('dominionFrontendApp');
 
 dominionFrontendApp.component('login', {
   template: `
-    <div class="form">
-      <p>
-        <label for="username">Username</label><br>
-        <input type="text" id="username" ng-model="user.username">
-      </p>
-      <p>
-        <label for="password">password</label><br>
-        <input type="password" id="password" ng-model="user.password">
-      </p>
+    <div class="form" class="navbar-form">
+      <input type="text" id="username" ng-model="user.username" class="form-control mr-sm-2" placeholder="Username" aria-label="Username">
+      <input type="password" id="password" ng-model="user.password" class="form-control mr-sm-2" placeholder="Password" aria-label="Password">
       <button ng-click="login()">Login</button>
     </div>`,
   controller: function($http, $resource, $route, $location, baseUrl, jwtService) {
