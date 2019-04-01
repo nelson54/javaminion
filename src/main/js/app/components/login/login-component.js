036fb8a704fe828a93a3446ffed73c6b5cb5404c
Derek Nelson <dcnelson@zixcorp.com>
2019-04-01 10:48:04 -0400

    Upgrading to angular 1.5 - fixed.

--- src/main/js/app/components/login/login-component.js
+++ src/main/js/app/components/login/login-component.js
@@ -1,24 +1,35 @@
 'use strict';
 
-let dominionFrontendApp = angular.module('dominionFrontendApp', ['$http', '$resource', '$route', '$location', 'baseUrl', 'jwtService'])
-dominionFrontendApp.component('home', {
-  template: '<h1>Home</h1><p>Hello, {{ $ctrl.user.name }} !</p>',
-  controller: function() {
-    this.user = {name: 'world'};
-  }
-})
-  .controller('LoginCtrl', function ($scope, $http, $resource, $route, $location, baseUrl, jwtService) {
-    let self = $scope;
+let dominionFrontendApp = angular.module('dominionFrontendApp');
+
+dominionFrontendApp.component('login', {
+  template: `
+    <div class="form">
+      <p>
+        <label for="username">Username</label><br>
+        <input type="text" id="username" ng-model="user.username">
+      </p>
+      <p>
+        <label for="password">password</label><br>
+        <input type="password" id="password" ng-model="user.password">
+      </p>
+      <button ng-click="login()">Login</button>
+    </div>`,
+  controller: function($http, $resource, $route, $location, baseUrl, jwtService) {
+
     let Authentication = $resource(baseUrl+'/api/authentication');
 
-    self.user = new Authentication({});
+    this.user = new Authentication({});
 
-    self.login = function() {
-      self.user.$save().then(function(response) {
+    this.isLoggedIn = ()=>{
+      return !!jwtService.getBearer();
+    };
+
+    this.login = ()=>{
+      this.user.$save().then(function(response) {
         jwtService.setToken(response.token);
         $location.path('/games');
       });
     };
-
-
-  });
+  }
+});
